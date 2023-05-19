59228c3a1fe0787a98fc60ad5a38384dc498d0f1
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-19 11:21:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+
 function highlight() {
     //Write your code here
+	const strongTags = document.querySelectorAll("strong");
 
-
+	for(const tags of strongTags){
+		tags.style.color = "green";
+	}
 }
 
 
 function return_normal() {
     //Write your code here
+	const strongTags = document.querySelectorAll("strong");
 
+	for(const tags of strongTags){
+		tags.style.color = "black";
+	}
     
 }
